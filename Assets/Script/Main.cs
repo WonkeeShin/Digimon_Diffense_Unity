fa7fed0f72db444c6bbcad93cea2dfc1461d3ed0
WonkeeShin <vkstk0056@naver.com>
2021-05-27 21:47:05 +0900

    디지몬의 기본 데이터를 csv에 도감처럼 작성. 그 후 불러오는 작업 완료. 게임 시작하면 로딩 중에 Loader에서 각 디지몬 한테 값을 쏴주면 될듯.

---
+++ Assets/Script/Main.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using Bolt;
+
+public class Main : MonoBehaviour
+{
+	private GameObject Digimon;
+	public int Digi_NO;
+	public string Digi_Name;
+	public int Digi_HP;
+	public int Digi_ATK;
+	public int Digi_DF;
+	public int Digi_Cost;
+	public int Digi_Colli_NO;
+	public float Digi_ATK_Speed;
+	public int Digi_Retime;
+	public float Digi_Speed;
+
+	void Awake()
+	{
+
+		List<Dictionary<string, object>> data = CSVReader.Read("Digimon_Data");
+		Digi_NO = (int)data[0]["Number"];
+		Digi_Name = (string)data[0]["Name"];
+		Digi_HP = (int)data[0]["HP"];
+		Digi_ATK = (int)data[0]["ATK"];
+		Digi_DF = (int)data[0]["DF"];
+		Digi_Cost = (int)data[0]["Cost"];
+		Digi_Colli_NO = (int)data[0]["Colli_No"];
+		Digi_ATK_Speed = (int)data[0]["ATK_Speed"];
+		Digi_Retime = (int)data[0]["ReTime"];
+		Digi_Speed = (int)data[0]["Speed"];
+		Digimon = GameObject.Find("Botamon");
+		Variables.Object(Digimon).Set("basic_data", data[0]);
+
+
+	}
+
+	// Use this for initialization
+	void Start()
+	{
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+
+	}
+}
