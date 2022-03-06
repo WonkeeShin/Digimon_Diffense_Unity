b278b9f7e564fc5f8aa51c80f65fb65e559a530e
WonkeeShin <vkstk0056@naver.com>
2022-03-06 21:30:14 +0900

    패널이 on 되었을 때 관련 애니메이션이 시작되도록. 배치 되지 않을 때 효과음도

---
+++ Assets/Script/Panel_Frame_OFF.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Panel_Frame_OFF : MonoBehaviour
+{
+    public void FrameOff()
+    {
+        gameObject.SetActive(false);
+    }
+
+}
