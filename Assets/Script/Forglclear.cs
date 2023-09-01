17516dd0b63304fa594eff59fee36298f44d1ab2
WonkeeShin <vkstk0056@naver.com>
2023-09-01 23:28:43 +0900

    해상도별 ui 대응. 및 깨지는 화면 gl.clear 라이트 유저용 디지몬 스탯 별모양으로 표시.

---
+++ Assets/Script/Forglclear.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Bolt;
+
+public class Forglclear : MonoBehaviour
+{
+    private GameObject Camera;
+ 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Camera = GameObject.FindWithTag("MainCamera");
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if ((bool)Variables.Object(Camera).Get("RectOn"))
+        {
+            GL.Clear(true, true, Color.black);
+        }
+
+    }
+
+}
