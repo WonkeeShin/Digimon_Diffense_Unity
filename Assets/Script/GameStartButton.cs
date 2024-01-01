ac6a3d5d44b2467e6c88858741b93a6e805272a9
WonkeeShin <vkstk0056@naver.com>
2024-01-01 23:23:40 +0900

    1. 문장이미지 만들기 8개의 문장 만들음. 빛의 문장, 희망의 문장을 선택할 수 있을 때와 없을 때로 구별 하여 만들음 선택할 수 있는 문장 : 색으로 표현되는 문장 선택할 수 없는 문장(빛, 희망) : 회색에 선택할 수 없게 표현.(퀘스트 완료 후 변경) 이미 보유 중인 문장 : 흰색으로 선택할 수 없게 표현됨
    2. 표지화면 만들기
    표지 화면에 어울리는 선택 버튼 만들기
    ‘게임시작’
    ‘상점’
    ‘도감’
    일단 다음 씬으로 넘어가는 거까진 해놓음
     > 게임 시작 시 바로 다음 씬으로 가지 말고 효과음이나 메시지 뜨고 넘어가도 좋을듯

---
+++ Assets/Script/GameStartButton.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Bolt;
+
+public class GameStartButton : MonoBehaviour
+{
+    private GameObject Next_Scene;
+    public bool MapOkBool;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    void Awake()
+    {
+        Next_Scene = GameObject.Find("Game_Start_button");
+        MapOkBool = (bool)Variables.Object(Next_Scene).Get("Map_Go_Ok_Bool");
+    }
+
+    void OnMouseDown()
+    {
+        MapOkBool = (bool)Variables.Object(Next_Scene).Get("Map_Go_Ok_Bool");
+        if (MapOkBool)
+        {
+            SceneLoader.Instance.LoadScene("Map_Select_Menu");
+        }
+
+
+
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
