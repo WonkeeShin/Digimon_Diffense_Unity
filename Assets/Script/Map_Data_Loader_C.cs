1eb638ac79fb9bded66645bd16014b8fd53f5ec4
WonkeeShin <vkstk0056@naver.com>
2024-02-04 23:12:30 +0900

    오늘 한 것 맵 컨트롤 체계 변경 중 Total_Controller에서 전체 통제 Map Controller Map Container 같은 난이도 맵 중에서 랜덤으로 뜨도록 설정(한 게임 안에서는 고정) 한 게임에 3개의 맵을 만난다면 4개까지는 만들어놓기. 즉, 여유분 1개 맵의 정보 ‘난이도 01’이 화면 따라다닐 수 있도록 함.
    다음 할 것
    맵 난이도 고려하여 여러개 만들어 놓기

---
+++ Assets/Script/Map_Data_Loader_C.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using Bolt;
+
+public class Map_Data_Loader_C : MonoBehaviour
+{
+	private GameObject Map_Loader;
+	public int Map_NO;
+	public int Map_Chapter;
+	public int Map_Order;
+	public int Map_Level;
+	public int Map_Special;
+	public int Map_UpDown;
+	public int Map_Hard;
+	private List<string> list = new List<string>();
+	public bool Map_Data_Loader_Bool;
+
+	void Awake()
+	{
+		Map_Loader = GameObject.Find("Map_Data_Loader");
+
+		List<Dictionary<string, object>> data = CSVReader.Read("Map_Data");
+
+		list.Add("Chap0_Map1");
+		list.Add("Chap0_Map2");
+		list.Add("Chap0_Map3");
+		list.Add("Chap0_Map4");
+		list.Add("Chap0_Map5");
+		list.Add("Chap0_Map6");
+		list.Add("Chap0_Map7");
+		list.Add("Chap0_Map8");
+		list.Add("Chap0_Map9");
+		list.Add("Chap0_Map10");
+		list.Add("Chap0_Map11");
+		list.Add("Chap0_Map12");
+		list.Add("Chap0_Map13");
+		list.Add("Chap0_Map14");
+
+
+		for (int i = 0; i < 14; i++)
+		{
+			Map_NO = (int)data[i]["Number"];
+			Map_Chapter = (int)data[i]["Chapter"];
+			Map_Order = (int)data[i]["Order"];
+			Map_Level = (int)data[i]["Level"];
+			Map_Special = (int)data[i]["Special"];
+			Map_UpDown = (int)data[i]["Up"];
+			Map_Hard = (int)data[i]["Hard"];
+
+			Variables.Object(Map_Loader).Set(list[i], data[i]);
+		}
+
+		Map_Data_Loader_Bool = (bool)true;
+		Variables.Object(Map_Loader).Set("Map_Data_Loader_Bool", Map_Data_Loader_Bool);
+
+	}
+}
