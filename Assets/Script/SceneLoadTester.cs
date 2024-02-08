5e61423151b82b411e8aa0b44f42c7edfa01f512
WonkeeShin <vkstk0056@naver.com>
2024-02-09 02:16:10 +0900

    1-1맵을 배껴서 다른 맵들 만들기 쌉가능 적군 디지몬도 그냥 이름과 animator만 바끄면됨. 그러고 맵 배경 조절하고 pallete 조절하면 맵 1개 뚝닥

--- Assets/Script/SceneLoadTester.cs
+++ Assets/Script/SceneLoadTester.cs
@@ -7,6 +7,7 @@ public class SceneLoadTester : MonoBehaviour
 {
     private GameObject Next_Scene;
     public bool MapOkBool;
+    public string Number_Level;
 
     // Start is called before the first frame update
     void Start()
@@ -18,14 +19,16 @@ public class SceneLoadTester : MonoBehaviour
     {
         Next_Scene = GameObject.Find("Next_Scene_button");
         MapOkBool = (bool)Variables.Object(Next_Scene).Get("Map_Go_Ok_Bool");
+        Number_Level = (string)Variables.Object(Next_Scene).Get("Determined_Stage_Name");
     }
 
     void OnMouseDown()
     {
         MapOkBool = (bool)Variables.Object(Next_Scene).Get("Map_Go_Ok_Bool");
+        Number_Level = (string)Variables.Object(Next_Scene).Get("Determined_Stage_Name");
         if (MapOkBool)
         {
-            SceneLoader.Instance.LoadScene("Map_00");
+            SceneLoader.Instance.LoadScene("Map"+Number_Level);
         }
         
   
